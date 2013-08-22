7c00de9532d7b72663ee1191c94523bc2c02e5d3
gami-pon <ken.yamagami.05@gmail.com>
2013-08-22 13:55:45 +0900

    test.py

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 import cgi
- 
-form = cgi.FieldStorage()
-x = form["x"].value
-y = form["y"].value
- 
-print '''Content-type: text/html\n
+import cgitb; cgitb.enable()
+
+html = '''Content-type: text/html\n
 <html>
-<body><h1>'''
-
-print x+", "+y
-
-print '''</h1></body>
+<body><h1>
+%d
+</h1></body>
 </html>
+'''
+
+form = cgi.FieldStorage()
+pos = [form["x"].value, form["y"].value]
+
+hoge = int(pos[0])+int(pos[1])
+
+print html % hoge
+
+'''
+if not (form.has_key("x") and form.has_key("y")):
+    print "<H1>Error</H1>"
+    print "Please fill in the name and addr fields."
+    return
 '''
