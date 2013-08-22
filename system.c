8f71c9d6fc1d8129943119598fbbe3921727b055
Tksenda <tksendamasato@gmail.com>
2013-08-22 12:15:55 +0900

    人口増加機能を追加

--- system.c
+++ system.c
@@ -22,7 +22,30 @@ int main(void){
 		hue_1_x=temple_x[k]-4;
 		hue_1_y=temple_y[k]-4;
 		hue_2_x=temple_x[k]+4;
-		if()
+		hue_2_y=temple_y[k]+4;
+		if(hue_1_x<0){
+			hue_1_x=0;
+		}else if(11<hue_1_x && hue_1_x<20){
+			hue_1_x=20;
+		}
+		if(hue_1_y<0){
+			hue_1_y=0;
+		}else if(11<temple_x[k] && temple_x[k]<20 && hue_1_y<10){
+			hue_1_y=10;
+		}
+		if(31<hue_2_x){
+			hue_2_x=31;
+		}else if(11<hue_2_x && hue_2_x<20){
+			hue_2_x=11;
+		}
+		if(37<hue_2_y){
+			hue_2_y=37;
+		}
+		for(i=hue_1_y;i<hue_2_y;i++){
+			for(j=hue_1_x;j<hue_2_x;j++){
+				population[j][i]+=100;
+			}
+		}
 	}
 	for(i=0;i<38;i++){
 		for(j=0;j<32;j++){
